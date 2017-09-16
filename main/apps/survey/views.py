bba1dc3d1b04077f0a4cd565de2e5e6ccfb8fd97
Jim <jim.pollaro@gmail.com>
2017-09-15 17:01:11 -0700

    can't get to process

--- main/apps/survey/views.py
+++ main/apps/survey/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+def index(request):
+    if 'count' not in request.session or request.session['count'] == 0:
+        request.session['count'] = 1
+    return render(request,'survey/index.html')
+
+def process(request):
+    request.session['name'] = request.post['name']
+    request.session['loc'] = request.post['loc']
+    request.session['language'] = request.post['language']
+    request.session['comments'] = request.post['comments']
+    request.session['count'] += 1
+    print request.session
+    return redirect(result)
+
+def result(request):
+    return render(request,'survey/result.html')
